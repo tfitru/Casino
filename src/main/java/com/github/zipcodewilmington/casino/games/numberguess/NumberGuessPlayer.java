c7825d1bb8be2ddce9b5220c1b6b02d7759027f0
marybunde <marybunde@gmail.com>
2022-07-18 20:48:18 -0400

    number guess game done

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessPlayer.java
@@ -1,8 +1,11 @@
 package com.github.zipcodewilmington.casino.games.numberguess;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import com.github.zipcodewilmington.casino.PlayerInterface;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -10,11 +13,49 @@ import java.util.List;
  * Created by leon on 7/21/2020.
  */
 
-public class NumberGuessPlayer{
+public class NumberGuessPlayer extends CasinoAccountManager implements PlayerInterface {
+    List<CasinoAccount> holdPlayer = new ArrayList<>();
+    Object casinoAccount;
+    Integer balance;
 
-    public NumberGuessPlayer(){
-     //List<CasinoAccount> NumberGuessPlayer = new List<CasinoAccount>();
+    public NumberGuessPlayer(CasinoAccount casinoAccount){
+//        List<CasinoAccount> NumberGuessPlayer = new List<CasinoAccount>();
+        this.casinoAccount = casinoAccount;
+        this.balance = casinoAccount.getBalance();
+    }
 
+    @Override
+    public CasinoAccount getArcadeAccount() {
+        return (CasinoAccount) this.casinoAccount;
+    }
+
+    @Override
+    public <SomeReturnType> SomeReturnType play() {
+        return null;
+    }
+
+    @Override
+    public void Garbage() {
+
+    }
+
+    @Override
+    public void CasinoAccount() {
+
+    }
+
+    @Override
+    public void setBet() {
+
+    }
+
+    @Override
+    public void cashIn() {
+
+    }
+
+    @Override
+    public void cashOut() {
 
     }
 }
